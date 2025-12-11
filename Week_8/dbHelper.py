dc11dc1dc7511a6c4b5fd83172b2f41bd89a9faa
Fadil <FA1325@live.mdx.ac.uk>
2025-12-11 15:34:53 +0400

    Delete Week_8 directory

--- Week_8/dbHelper.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from pathlib import Path
-
-DATA_DIR = Path("DATA")
-DB_PATH = DATA_DIR / "cyberincidents.db"
-
-def connect_database(db_path=DB_PATH):
-    return sqlite3.connect(str(db_path))
